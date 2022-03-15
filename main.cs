262fdcbc6dcd750fe204578fbd3e471ae712c055
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-03-15 18:53:52 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+// Created by: Zaida Hammel
+// Created on: Sep 2020
+//
+// This program displays, "Hello, World!"
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+    Console.WriteLine ("/nDone.");
+  }
+}
